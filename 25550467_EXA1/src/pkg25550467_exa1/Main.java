cf384309fc30e5eb9ac09c2ed880d7a1e04befb4
Antonio-Vidal1025 <L25550647@chihuahua2.tecnm.mx>
2025-09-30 17:03:49 -0600

    Add files via upload

---
+++ 25550467_EXA1/src/pkg25550467_exa1/Main.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg25550467_exa1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Antonio
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    final static String PWD = "1234";
+    public static void main(String[] args) {
+    String usu, contra;
+    Scanner input = new Scanner (System.in);
+    
+int saldo = 5000;
+    
+    System.out.println("Entrada al cajero de banorte");
+    System.out.println("Usuario ingrese su contraseña: ");
+    usu = input.nextLine();
+    if(usu.equals(PWD)) {//contraseña correcta
+        System.out.println("ACCESO CONCEDIDO");
+    }else{//contraseña incorrecta 
+        System.out.println("ACCESO DENEGADO");
+    }
+   
+    System.out.println("Menú elije una opción: 1 2 3 ");
+    saldo = input.nextInt();
+            Saldo = 5000
+    if(1)
+        }
+      }    
+
+    
+    
